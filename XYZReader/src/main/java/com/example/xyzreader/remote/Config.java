0ef6c1dba7cf99cb33bc7d23e49d5e1a283e13d0
Srey Seng <sreyseng@gmail.com>
2018-08-21 01:47:55 -0700

    Update json url

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -14,7 +14,7 @@ public class Config {
     URL url = null;
     try {
       url = new URL(
-          "https://s3-us-west-2.amazonaws.com/44d827ed-8529-42b0-98fb-eb9d2afd6d12/data/xyz-reader.json");
+          "https://go.udacity.com/xyz-reader-json");
     } catch (MalformedURLException ignored) {
       // TODO: throw a real error
       Log.e(TAG, "Please check your internet connection.");
